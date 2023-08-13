dcf91a08a52996721327bb47e6c53954991f3dc7
Salmandotweb <salmandotweb@gmail.com>
2023-08-13 20:02:44 +0500

    adding remaining sections to prisma template

--- src/templates/Prisma/index.tsx
+++ src/templates/Prisma/index.tsx
@@ -10,13 +10,15 @@ import { useAppSelector } from "@/stores/hooks";
 import { selectBasicInfo } from "@/stores/slices/basic/basicInfoSlice";
 import { BsCalendar2Minus } from "react-icons/bs";
 import { Badge } from "@/components/ui/badge";
+import { BsLink45Deg } from "react-icons/bs";
+import { BsStars } from "react-icons/bs";
 
 interface indexProps {}
 
 const Prisma: FC<indexProps> = ({}) => {
 	const basicInfo = useAppSelector(selectBasicInfo);
 	return (
-		<div className="bg-[#0F172A] w-[230mm] h-[296mm] text-white overflow-hidden">
+		<div className="bg-[#0F172A] w-[230mm] min-h-[296mm] text-white overflow-hidden mb-[50px] pb-10">
 			<div className="flex items-center justify-center flex-col h-[200px] w-full relative z-[100]">
 				<Gradient />
 				<h1 className="text-[36px] text-white font-bold">{basicInfo.name}</h1>
@@ -76,6 +78,56 @@ const Prisma: FC<indexProps> = ({}) => {
 						<Experience />
 					</div>
 				</div>
+				<Separator className="bg-[#334155]" />
+				<div className="grid grid-cols-[1fr,4.5fr] gap-3 items-start w-full">
+					<h1 className="text-[#E2E8F0] text-[16px] font-medium">
+						Featured Project
+					</h1>
+					<div className="flex flex-col gap-2">
+						<div className="grid grid-cols-[2fr,1fr] gap-3 items-start">
+							<h1 className="text-[#E2E8F0] text-[16px] font-medium">
+								Nexcare.io
+							</h1>
+						</div>
+						<div className="flex items-center justify-start gap-3">
+							<CustomBadge label="Next.Js" />
+							<CustomBadge label="Typescript" />
+							<CustomBadge label="Material UI" />
+							<CustomBadge label="PHP" />
+							<CustomBadge label="Laravel" />
+							<CustomBadge label="MySql" />
+						</div>
+						<div className="flex flex-col gap-3 justify-start items-start mt-2">
+							<Summary
+								summary="NexCare is an integrated solution for single and multi-specialty practices that optimizes care delivery and empowers doctors to focus on the patient. More details at"
+								link="https://nexcare.io/"
+							/>
+
+							<div className="flex items-center gap-2 mt-2">
+								<BsStars />{" "}
+								<p className="text-[#CBD5E1] text-[14px] font-light">
+									See all of my projects on my portfolio:{" "}
+								</p>
+								<span className="underline inline-flex items-center gap-1 text-[#CBD5E1] text-[14px] font-light">
+									<BsLink45Deg /> https://salmandotweb.me/projects
+								</span>
+							</div>
+						</div>
+					</div>
+				</div>
+				<Separator className="bg-[#334155]" />
+				<div className="grid grid-cols-[1fr,4.5fr] gap-3 items-start w-full">
+					<h1 className="text-[#E2E8F0] text-[16px] font-medium">
+						Skills and Tools
+					</h1>
+					<div className="flex flex-col gap-3">
+						<Skills />
+						<Separator className="bg-[#334155]" />
+						<Skills />
+						<Separator className="bg-[#334155]" />
+						<Skills />
+					</div>
+				</div>
 			</div>
 		</div>
 	);
@@ -113,6 +165,31 @@ const Experience = () => {
 	);
 };
 
+const Skills = () => {
+	return (
+		<div className="flex items-start gap-3 flex-col">
+			<h1 className="text-[#E2E8F0] text-[16px] font-medium">Languages</h1>
+			<div className="flex items-center gap-5">
+				<p className="text-[#64748B] text-[14px]">Frontend related</p>
+				<div className="flex items-center gap-2">
+					<CustomBadge label="Javascript" />
+					<CustomBadge label="React.Js" />
+					<CustomBadge label="Next.Js" />
+					<CustomBadge label="Typescript" />
+				</div>
+			</div>
+			<div className="flex items-center gap-5">
+				<p className="text-[#64748B] text-[14px]">Backend related</p>
+				<div className="flex items-center gap-2">
+					<CustomBadge label="Node.Js" />
+					<CustomBadge label="TRPC" />
+					<CustomBadge label="Postgresql" />
+				</div>
+			</div>
+		</div>
+	);
+};
+
 const CustomBadge = ({ label }: { label: string }) => {
 	return (
 		<Badge
@@ -123,11 +200,18 @@ const CustomBadge = ({ label }: { label: string }) => {
 	);
 };
 
-const Summary = ({ summary }: { summary: string }) => {
+const Summary = ({ summary, link }: { summary: string; link?: string }) => {
 	return (
 		<div className="flex items-start gap-2 justify-start">
 			<div className="h-[10px] w-[12px] bg-[#CBD5E1] rounded-[3px] mt-[4px]" />
-			<p className="text-[#CBD5E1] text-[14px] font-light">{summary}</p>
+			<p className="text-[#CBD5E1] text-[14px] font-light">
+				{summary}{" "}
+				{link && (
+					<span className="underline inline-flex items-center gap-1">
+						<BsLink45Deg /> {link}
+					</span>
+				)}
+			</p>
 		</div>
 	);
 };
