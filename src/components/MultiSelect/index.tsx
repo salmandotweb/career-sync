142fe61b99f117b2fe9876f0f34968d551eed144
Salmandotweb <salmandotweb@gmail.com>
2023-08-17 08:34:20 +0500

    refactoring

--- src/components/MultiSelect/index.tsx
+++ src/components/MultiSelect/index.tsx
@@ -2,24 +2,37 @@ import { Fragment, useState } from "react";
 import { Combobox, Transition } from "@headlessui/react";
 import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
 
-const people = [
-	{ id: 1, name: "Wade Cooper" },
-	{ id: 2, name: "Arlene Mccoy" },
-	{ id: 3, name: "Devon Webb" },
-	{ id: 4, name: "Tom Cook" },
-	{ id: 5, name: "Tanya Fox" },
-	{ id: 6, name: "Hellen Schmidt" },
+const skills = [
+	{ id: 1, name: "JavaScript" },
+	{ id: 2, name: "TypeScript" },
+	{ id: 3, name: "React" },
+	{ id: 4, name: "Next.js" },
+	{ id: 5, name: "Node.js" },
+	{ id: 6, name: "Express" },
+	{ id: 7, name: "GraphQL" },
+	{ id: 8, name: "Apollo" },
+	{ id: 9, name: "Prisma" },
+	{ id: 10, name: "PostgreSQL" },
+	{ id: 11, name: "MongoDB" },
+	{ id: 12, name: "Redis" },
+	{ id: 13, name: "Tailwind CSS" },
+	{ id: 14, name: "Chakra UI" },
+	{ id: 15, name: "Material UI" },
+	{ id: 16, name: "Jest" },
+	{ id: 17, name: "React Testing Library" },
+	{ id: 18, name: "Cypress" },
+	{ id: 19, name: "React Hook Form" },
 ];
 
 export default function Example() {
-	const [selected, setSelected] = useState(people[0]);
+	const [selected, setSelected] = useState(skills[0]);
 	const [query, setQuery] = useState("");
 
-	const filteredPeople =
+	const filteredSkills =
 		query === ""
-			? people
-			: people.filter((person) =>
-					person.name
+			? skills
+			: skills.filter((skill) =>
+					skill.name
 						.toLowerCase()
 						.replace(/\s+/g, "")
 						.includes(query.toLowerCase().replace(/\s+/g, ""))
@@ -51,36 +64,28 @@ export default function Example() {
 						leaveTo="opacity-0"
 						afterLeave={() => setQuery("")}>
 						<Combobox.Options className="relative z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2">
-							{filteredPeople.length === 0 && query !== "" ? (
+							{filteredSkills.length === 0 && query !== "" ? (
 								<div className="px-2 py-1.5 text-sm font-medium">
 									Nothing found.
 								</div>
 							) : (
-								filteredPeople.map((person) => (
+								filteredSkills.map((skill) => (
 									<Combobox.Option
-										key={person.id}
+										key={skill.id}
 										className={({ active }) =>
 											`relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-2 pr-8 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50 ${
 												active ? "bg-gray-300 text-black" : "text-gray-600"
 											}`
 										}
-										value={person}>
-										{({ selected, active }) => (
+										value={skill}>
+										{({ selected }) => (
 											<>
 												<span
 													className={`block truncate ${
 														selected ? "font-medium" : "font-normal"
 													}`}>
-													{person.name}
+													{skill.name}
 												</span>
-												{selected ? (
-													<span
-														className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
-															active ? "text-white" : "text-teal-600"
-														}`}>
-														<CheckIcon className="h-5 w-5" aria-hidden="true" />
-													</span>
-												) : null}
 											</>
 										)}
 									</Combobox.Option>
