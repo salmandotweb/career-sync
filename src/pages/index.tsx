979b023ec2a9e3ced4656274039e1bcf7d5c842d
Salmandotweb <salmandotweb@gmail.com>
2023-09-24 19:32:57 +0500

    features section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/Layout/Navbar";
+import Features from "@/components/Sections/Features";
 import Hero from "@/components/Sections/Hero";
 import Head from "next/head";
 
@@ -13,6 +14,7 @@ export default function Home() {
 			<div className="px-24">
 				<Navbar />
 				<Hero />
+				<Features />
 			</div>
 		</>
 	);
